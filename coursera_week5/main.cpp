b68b85bce3b988b95d6bb24992a960558dc82b97
Yosuke Yasuda <double.y.919.quick@gmail.com>
2015-11-14 11:00:25 -0800

    Initial Commit

---
+++ coursera_week5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  coursera_week5
+//
+//  Created by 安田洋介 on 11/14/15.
+//  Copyright © 2015 安田洋介. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
